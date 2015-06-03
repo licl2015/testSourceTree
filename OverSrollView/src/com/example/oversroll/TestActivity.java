386f0f3d54df6935566f2bb161733f7667efd129
licl <lcl11718@ly.com>
2015-06-03 12:29:36 +0800

    添加本地修改

--- OverSrollView/src/com/example/oversroll/TestActivity.java
+++ OverSrollView/src/com/example/oversroll/TestActivity.java
@@ -9,6 +9,10 @@ public class TestActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
+		initView();
+	}
+	public void initView(){
+		
 	}
 
 }
